0e01f79ce2c12b7def4921d9e1eb559b25b3fd76
Mahmud <ajaomahmud@gmail.com>
2021-05-30 23:11:21 +0100

    chore: Add jwt guard to controller

--- src/modules/addons/addons.controller.ts
+++ src/modules/addons/addons.controller.ts
@@ -6,12 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AddOnsService } from './addons.service';
 import { CreateAddonDto } from './dto/create-addon.dto';
 import { UpdateAddonDto } from './dto/update-addon.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('addons')
+@UseGuards(JwtAuthGuard)
 export class AddOnsController {
   constructor(private readonly addonsService: AddOnsService) {}
 
