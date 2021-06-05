91c9ad4dac030a6203f1a93b82596c3a738a8cd0
Mahmud <ajaomahmud@gmail.com>
2021-06-05 18:03:43 +0100

    fix: fixed jwt payload error

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../../users/users.service';
+import {JwtPayload} from "../interfaces/jwt-payload";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,9 +14,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JwtPayload) {
     // check if user in the token actually exist
-    const user = await this.userService.findOneById(payload.sub);
+    const user = await this.userService.findOneById(payload.id);
     if (!user) {
       throw new UnauthorizedException(
         'You are not authorized to access this resource',
