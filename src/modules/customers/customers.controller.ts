754df761621a1f35acfc3f3e1e3397a1f71840ec
Mahmud <ajaomahmud@gmail.com>
2021-06-05 18:06:34 +0100

    chore: Modify response object

--- src/modules/customers/customers.controller.ts
+++ src/modules/customers/customers.controller.ts
@@ -11,12 +11,14 @@ import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { IsCustomerExistGuard } from './guards/isCustomerExist.guard';
 
 @Controller('customers')
 @UseGuards(JwtAuthGuard)
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
+  @UseGuards(IsCustomerExistGuard)
   @Post()
   async create(@Body() createCustomerDto: CreateCustomerDto) {
     const customer = await this.customersService.create(createCustomerDto);
@@ -25,12 +27,14 @@ export class CustomersController {
 
   @Get()
   async findAll() {
-    return this.customersService.findAll();
+    const customers = await this.customersService.findAll();
+    return { message: 'Data retrieved', result: customers };
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.customersService.findOne(id);
+    const customer = await this.customersService.findOneById(id);
+    return { message: 'Data retrieved', result: customer };
   }
 
   @Patch(':id')
