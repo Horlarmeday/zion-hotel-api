2d2741896cb5711a9cf864dcd186365ba22a7190
Mahmud <ajaomahmud@gmail.com>
2021-06-15 10:30:49 +0100

    feat: Add create payment method in controller

--- src/modules/accounts/accounts.controller.ts
+++ src/modules/accounts/accounts.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { QueryDto } from '../../core/pipes/query-dto';
+import { CreateAccountDto } from './dto/create-account.dto';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
+
+  @Post(':id')
+  async createPayment(
+    @Body() createAccountDto: CreateAccountDto,
+    @Param('id') id: string,
+  ) {
+    const payment = await this.accountsService.createPayment(
+      createAccountDto,
+      id,
+    );
+    return { message: 'Payment saved', result: payment };
+  }
 
   @Get()
   async findAll(@Query() queryDto: QueryDto) {
